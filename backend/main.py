b4336856b8b3c5411dab206ea071c910f9f9f24b
naishagarwal <naisha1025@gmail.com>
2025-02-15 17:43:42 -0800

    api changes

--- backend/main.py
+++ backend/main.py
@@ -26,19 +26,12 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 #genai_client = genai.Client(api_key=GEMINI_API_KEY)
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
-# class ChildProfile(BaseModel):
-#     name: str
-#     age: int
-#     gender: str
-#     location: str
-#     race: str
-#     interests: str
-
 class Profile(BaseModel):
     nickname: str
     age: int
     location: str
     gender: str
+    race: str
     interests: str
 
 class StoryRequest(BaseModel):
